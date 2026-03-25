2e217fd09d28d2a652925300718972a53f4d5f98
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-25 19:13:31 -0400

    Added basic operators and operate function

---
+++ calculator.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function subtract(num1, num2) {
+    return num1 - num2
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+function operate(operator, num1, num2) {
+    switch (operator.toLowerCase()) {
+        case 'add':
+            result = add(num1, num2)
+            break;
+        case 'subtract':
+            result = subtract(num1, num2)
+            break;
+        case 'multiply':
+            result = multiply(num1, num2)
+            break;
+        case 'divide':
+            result = divide(num1, num2)
+            break;
+    }
+    return result
+}
