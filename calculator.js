14d8f7b8addfdc76a32bdd3c92383bd7954e45f2
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-25 19:45:20 -0400

    Setup event listener for each button

--- calculator.js
+++ calculator.js
@@ -31,3 +31,20 @@ function operate(operator, num1, num2) {
     }
     return result
 }
+
+function updateDisplayAndReturnVar(variable) {
+    // If the variable is "Clear", set the p element to have no text
+    // Else, add the variable to the p element
+}
+
+// Need to return the value and also update the display
+// Special handling for clicking of clear?
+// Instead of update display it would clear it
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Call a seperate function to update display and return value
+        console.log(`Clicked button: ${button.innerText}`)
+    })
+})
